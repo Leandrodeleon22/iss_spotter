94bb57a686d9a6197d2c3f267113a489b3f07482
leandrodeleon22 <leandrodeleon22@gmail.com>
2023-11-20 08:26:10 -0500

    refactor issSpotter IV

--- index.js
+++ index.js
@@ -22,26 +22,26 @@ nextISSTimesForMyLocation((error, passtimes) => {
   printPassTimes(passtimes);
 });
 
-fetchMyIp((error, ip) => {
-  if (error) {
-    console.log("It didnt work", error);
-    return;
-  }
+// fetchMyIp((error, ip) => {
+//   if (error) {
+//     console.log("It didnt work", error);
+//     return;
+//   }
 
-  console.log("It worked! Returned IP:", ip);
-});
+//   console.log("It worked! Returned IP:", ip);
+// });
 
-fetchCoordsByIp("http://ipwho.is/99.241.135.239", (err, data) => {
-  if (err) {
-    console.log("It didnt work", err);
-    return;
-  }
-  console.log(data);
-  fetchISSFlyOverTimes(data, (error, arrayOfObj) => {
-    if (error) {
-      console.log("It didnt work", error);
-    }
-    printPassTimes(arrayOfObj);
-    // console.log(arrayOfObj);
-  });
-});
+// fetchCoordsByIp("http://ipwho.is/99.241.135.239", (err, data) => {
+//   if (err) {
+//     console.log("It didnt work", err);
+//     return;
+//   }
+//   console.log(data);
+//   fetchISSFlyOverTimes(data, (error, arrayOfObj) => {
+//     if (error) {
+//       console.log("It didnt work", error);
+//     }
+//     printPassTimes(arrayOfObj);
+//     // console.log(arrayOfObj);
+//   });
+// });
